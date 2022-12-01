bec5e09371098b2352f8a7272f9e87c041c10b7a
Milos Djakovic <milos.g.djakovic@gmail.com>
2022-12-01 11:45:37 +0100

    Added and configured following dependencies: 'astro-icon', 'astro-imagetools', and 'gsap'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -6,7 +6,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+// https://astro-imagetools-docs.vercel.app/en/introduction
+import { astroImageTools } from "astro-imagetools";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), astroImageTools],
 });
