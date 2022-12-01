9e16d5434b528b36eeebd048f8a0b2b33fcd5b04
Milos Djakovic <milos.g.djakovic@gmail.com>
2022-12-01 11:45:51 +0100

    Tailwind configuration updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "facebook-blue": "#1876f2",
+        "youtube-red": "#f80703",
+        "soundcloud-orange": "#f95502",
+        "mixcloud-purple": "#923eff",
+      },
+      height: {
+        72: "18rem",
+        84: "21rem",
+        96: "24rem",
+      },
+    },
+  },
+  plugins: [],
+};
