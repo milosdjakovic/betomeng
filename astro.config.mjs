7cde4b50e9d2b415e79664100d8e2bd52eae727f
Milos Djakovic <milos.g.djakovic@gmail.com>
2022-12-14 23:26:10 +0100

    Astro sitemap configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/
 import sitemap from "@astrojs/sitemap";
 
 // https://astro-imagetools-docs.vercel.app/en/introduction
@@ -11,5 +11,6 @@ import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://betomeng.com",
   integrations: [tailwind(), sitemap(), astroImageTools],
 });
